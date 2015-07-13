dbdbdd16b463fe56144d1c04185d393c90dc2c47
Joao Neves <joaoneves792@gmail.com>
2015-07-13 18:13:48 +0100

    Added an install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#Run me as root
+#enable the service after installation
+
+cp ./cd00rd.service /etc/systemd/system
+cp ./cd00rd /usr/bin/
+cp ./lock /usr/bin/
+
+id -u cd00r
+if [ $? -eq 1 ]; then
+	useradd -M -s /usr/bin/nologin cd00r
+fi
+
+chown root:root /usr/bin/cd00rd
+chown root:root /usr/bin/lock
+
+chmod u+s /usr/bin/lock
+setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/cd00rd
